459275f7f5f5a35be854959bfac593538004e372
oceane <oceane.gatien.cef@hotmail.fr>
2017-03-10 14:13:46 +0100

    ajout de Jquery=bouton jouer pour lancer le jeu

--- js/app.js
+++ js/app.js
@@ -1,24 +1,21 @@
+function jouer() {
+
 var solution = Math.random() * (3 - 0) + 0;
 var arrondis = Math.round(solution);
 
 
-var response = prompt("Ecrivez un chiffre entre 0 et 3");
+var response = prompt("But du jeu: écrivez un chiffre entre 0 et 3, et voyons si vous pensez comme la machine! 2 essais maximum! Good luck!");
 
 if (response < arrondis){
-	console.log("trop petit!");
-}else{
-	console.log("trop grand!");
+	var response = prompt("Trop petit!");
 }
-
-
+else{
+	var response = prompt("Trop Grand!");
+}
 if (response == arrondis){
-	console.log("Bravo! C'est gagné!");
+	alert("Bravo! C'est gagné!");
 } 
 else{
-	var response = prompt("Essayez encore !");
+	alert("Perdu! ");
 }
-
-
-
-
-
+}
